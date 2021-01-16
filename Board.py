0be83e0e8d220bb370a31ab66905a42cc12b2f6d
guillaume <guillaume.neirinckx@student.uclouvain.be>
2021-01-16 15:15:56 +0100

    add other pieces' moves

--- Board.py
+++ Board.py
@@ -31,8 +31,19 @@ class Board:
              Pieces.XX, Pieces.XX, Pieces.XX],
             [Pieces.XX, Pieces.XX, Pieces.XX, Pieces.XX, Pieces.XX, Pieces.XX, Pieces.XX, Pieces.XX, Pieces.XX,
              Pieces.XX, Pieces.XX, Pieces.XX]]
+
         self.is_white_piece = lambda piece_int: piece_int > 0
         self.is_black_piece = lambda piece_int: piece_int < 0
+
+        # castling rights
+        self.white_castled = False
+        self.black_castled = False
+        self.white_king_moved = False
+        self.black_king_moved = False
+        self.white_king_rook_moved = False
+        self.black_king_rook_moved = False
+        self.white_queen_rook_moved = False
+        self.black_queen_rook_moved = False
 
     def __str__(self):
         board_str = ""
@@ -76,33 +87,57 @@ class Board:
 
     def add_pawn_moves(self, pawn, is_white, is_enemy_evaluator, moves):  # todo en passant
         (row, col, is_pinned) = (pawn.row, pawn.col, pawn.is_pinned)
-        dir = [row - 1, row - 2, 8, row + 1, row + 2, 3]
+        target_row = [row - 1, row - 2, 8, row + 1, row + 2, 3]
         idx = 0 if is_white else 3
-        if row == dir[idx + 2] and self.board[dir[idx]][col] == Pieces.OO and self.board[dir[idx + 1]][
+        if row == target_row[idx + 2] and self.board[target_row[idx]][col] == Pieces.OO and self.board[target_row[idx + 1]][
             col] == Pieces.OO:
-            moves.append(Move(row, col, dir[idx + 1], col, self.board[dir[idx + 1]][col]))
-        if self.board[dir[idx]][col] == Pieces.OO and self.board[dir[idx]][col] == Pieces.OO:
-            moves.append(Move(row, col, dir[idx], col, self.board[dir[idx]][col]))
-        if self.is_in_bounds(dir[idx], col - 1) and is_enemy_evaluator(self.board[dir[idx]][col - 1]):
-            moves.append(Move(row, col, dir[idx], col - 1, self.board[dir[idx]][col - 1]))
-        if self.is_in_bounds(dir[idx], col + 1) and is_enemy_evaluator(self.board[dir[idx]][col + 1]):
-            moves.append(Move(row, col, dir[idx], col + 1, self.board[dir[idx]][col + 1]))
+            moves.append(Move(row, col, target_row[idx + 1], col, self.board[target_row[idx + 1]][col]))
+        if self.board[target_row[idx]][col] == Pieces.OO and self.board[target_row[idx]][col] == Pieces.OO:
+            moves.append(Move(row, col, target_row[idx], col, self.board[target_row[idx]][col]))
+        if self.is_in_bounds(target_row[idx], col - 1) and is_enemy_evaluator(self.board[target_row[idx]][col - 1]):
+            moves.append(Move(row, col, target_row[idx], col - 1, self.board[target_row[idx]][col - 1]))
+        if self.is_in_bounds(target_row[idx], col + 1) and is_enemy_evaluator(self.board[target_row[idx]][col + 1]):
+            moves.append(Move(row, col, target_row[idx], col + 1, self.board[target_row[idx]][col + 1]))
 
-    def add_rook_moves(self, rook, is_white, is_enemy_evaluator, moves):
-        pass
-
-    def add_knight_moves(self, knight, is_white, is_enemy_evaluator, moves):
-        pass
-
-    def add_bishop_moves(self, bishop, is_white, is_enemy_evaluator, moves):
-        pass
+    def add_knight_moves(self, knight, _is_white, is_enemy_evaluator, moves):
+        row, col = knight.row, knight.col
+        to_row = [row-1, row+1, row+2, row+2, row+1, row-1, row-2, row-2]
+        to_col = [col+2, col+2, col+1, col-1, col-2, col-2, col-1, col+1]
+        for i in range(0, len(to_row)):
+            to_square_val = self.board[to_row[i]][to_col[i]]
+            if (to_square_val == Pieces.OO or is_enemy_evaluator(to_square_val)) and to_square_val != Pieces.XX:
+                moves.append(Move(row, col, to_row[i], to_col[i], to_square_val))
 
     def add_queen_moves(self, queen, is_white, is_enemy_evaluator, moves):
         self.add_rook_moves(queen, is_white, is_enemy_evaluator, moves)
         self.add_bishop_moves(queen, is_white, is_enemy_evaluator, moves)
 
+    def add_rook_moves(self, rook, _is_white, is_enemy_evaluator, moves):
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        self.add_directed_moves(rook, directions, is_enemy_evaluator, moves)
+
+    def add_bishop_moves(self, bishop, _is_white, is_enemy_evaluator, moves):
+        directions = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
+        self.add_directed_moves(bishop, directions, is_enemy_evaluator, moves)
+
+    def add_directed_moves(self, piece, directions, is_enemy_evaluator, moves):
+        row, col = piece.row, piece.col
+        for direction in directions:
+            to_row, to_col = row+direction[0], col+direction[1]
+            while(self.board[to_row][to_col] == Pieces.OO):
+                moves.append(Move(row, col, to_row, to_col, self.board[to_row][to_col]))
+                to_row, to_col = to_row + direction[0], to_col + direction[1]
+            if is_enemy_evaluator(self.board[to_row][to_col]):
+                moves.append(Move(row, col, to_row, to_col, self.board[to_row][to_col]))
+
     def add_king_moves(self, king, is_white, is_enemy_evaluator, moves):
-        pass
+        row, col = king.row, king.col
+        to_squares = [(row+1, col-1), (row+1, col), (row+1, col+1), (row, col-1), (row, col+1), (row-1, col-1), (row-1, col), (row-1, col+1)]
+        for to_square in to_squares:
+            to_square_val = self.board[to_square[0]][to_square[1]]
+            if to_square_val == Pieces.OO or is_enemy_evaluator(to_square_val):
+                moves.append(Move(row, col, to_square[0], to_square[1], to_square_val))
+        # todo castle update castling rights + see if square attacked
 
     def do_move(self, move):  # todo queen pawn
         self.board[move.x2][move.y2] = self.board[move.x1][move.y1]
--- ChessGame.py
+++ ChessGame.py
@@ -7,20 +7,11 @@ class ChessGame:
     def __init__(self):
         self.board = Board()
         print(self.board)
-        # castling rights
-        self.white_castled = False
-        self.black_castled = False
-        self.white_king_moved = False
-        self.black_king_moved = False
-        self.white_king_rook_moved = False
-        self.black_king_rook_moved = False
-        self.white_queen_rook_moved = False
-        self.black_queen_rook_moved = False
 
         # list of pieces with their positions (x, y, is_pinned)
         (self.white_pieces, self.black_pieces) = self.board.init_pieces()
 
         moves, self.white_pieces = self.board.get_color_moves(self.white_pieces, True)
-        b = 2
+        print(len(moves))
 
 
